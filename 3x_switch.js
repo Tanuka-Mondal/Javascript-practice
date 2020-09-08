867380d0ed1ae7dcdf0303d24216839077304c1f
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2020-09-09 03:13:09 +0530

    Create 3x_switch.js

---
+++ 3x_switch.js
@@ -0,0 +1,25 @@
+function chainToSwitch(val) {
+  var answer = "";
+  
+  switch(val){
+    case "bob":
+      return "Marley";
+      break;
+    case 42:
+      return "The Answer";
+      break;
+    case 1:
+      return "There is no #1"
+      break;
+    case 99:
+      return "Missed me by this much!";
+      break;
+    case 7:
+      return "Ate Nine" 
+      break;       
+  }
+
+  return answer;
+}
+
+chainToSwitch(7);
