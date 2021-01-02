8aa9bcb0cefc396fc7a332451d1d1fd454dba3e3
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2021-01-03 00:22:26 +0530

    Create 4k Manipulating complex objects.js

---
+++ 4k Manipulating complex objects.js
@@ -0,0 +1,25 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add a record here
+  {
+    "artist": "Tom Hank",
+    "title": "The Life",
+    "release_year": 1967,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
